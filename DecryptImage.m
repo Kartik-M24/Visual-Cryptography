cc4b6c4e8b9d7eeb5fc0711ac07611194f9603b0
Kartik Malik <161120979+Kartik-M24@users.noreply.github.com>
2026-04-20 01:26:29 +1200

    Refactor DecryptImage function for clarity

--- DecryptImage.m
+++ DecryptImage.m
@@ -6,16 +6,16 @@
 % it is smaller then this is only done for pixels that overlap between the
 % key and the cipher image, to produce the decrypted image 'final_img'
 function final_img = DecryptImage(A,B)
-[a,b] = size(A); %determines the number of rows 'a' and columns 'b' in image 'A'
-[c,d] = size(B); %determines the number of rows 'c' and columns 'd' in image 'B'
-array = zeros(c,d); %makes an array of the same size as the key image
-array = uint8(array); %converts this array into a uint8 array
-for i = 1:a %goes through all rows
-    for j = 1:b %and goes through all columns
-        if i <= c %checks to see that the value of i is not greater than the number of rows in 'B'
-            if j <= d %checks to see that the value of j is not greater than the number of columns in 'B'
-                x = A(i,j); %x is assigned to the value of the pixel in row i column j of image 'A'
-                y = B(i,j); %y is assigned to the value of the pixel in row i column j of image 'B'
+[a,b] = size(A);
+[c,d] = size(B);
+array = zeros(c,d);
+array = uint8(array);
+for i = 1:a
+    for j = 1:b
+        if i <= c
+            if j <= d
+                x = A(i,j);
+                y = B(i,j);
                 %the following if statements check to see if x and y values have certain paried values if
                 %this is the case, the value of the pixel in the array is assigned a number (either 0 or
                 % 255) depending on the values of x and y
@@ -32,5 +32,5 @@ for i = 1:a %goes through all rows
         end
     end
 end
-final_img = array %the 'final_img' output is assigned to the completed array (after going through the loop)
+final_img = array
 end
